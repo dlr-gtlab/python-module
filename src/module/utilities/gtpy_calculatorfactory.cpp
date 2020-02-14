6d159d853bd8bf6c79eb42b91a64e7fd84a0d824
Nöthen, Marvin <marvin.noethen@dlr.de>
2020-02-14 08:19:36 +0100

    calculator factory allways returns a new calculator instance

--- src/module/utilities/gtpy_calculatorfactory.cpp
+++ src/module/utilities/gtpy_calculatorfactory.cpp
@@ -27,20 +27,20 @@ GtpyCalculatorFactory::createCalculator(const QString& className,
                                         const QString& objName,
                                         GtTask* parent)
 {
-    if (parent != Q_NULLPTR)
-    {
-        QList<GtCalculator*> calculators =
-                parent->findChildren<GtCalculator*>(objName);
+//    if (parent != Q_NULLPTR)
+//    {
+//        QList<GtCalculator*> calculators =
+//                parent->findChildren<GtCalculator*>(objName);
 
-        foreach (GtCalculator* calculator, calculators)
-        {
-            if (className == QString::fromUtf8(calculator->
-                                     metaObject()->className()))
-            {
-                return calculator;
-            }
-        }
-    }
+//        foreach (GtCalculator* calculator, calculators)
+//        {
+//            if (className == QString::fromUtf8(calculator->
+//                                     metaObject()->className()))
+//            {
+//                return calculator;
+//            }
+//        }
+//    }
 
     GtCalculatorData calcData =
         gtCalculatorFactory->calculatorData(className);
