b0a01445da8d68abac39a5e74948bc7ba6759c9a
Nöthen <Marvin.Noethen@dlr.de>
2020-04-29 10:29:49 +0200

    update the deployment to verion 1.3

--- tests/build/copyHeadersQt512Py373.bat
+++ tests/build/copyHeadersQt512Py373.bat
@@ -1,6 +1,8 @@
 SET NIGHTLYBUILD=G:\AT-TW\GTlab\Nightly_Builds_512
 
+if exist "%NIGHTLYBUILD%\include\python373\" rmdir /s /q %NIGHTLYBUILD%\include\python373\
 if not exist "%NIGHTLYBUILD%\include\python373\" mkdir %NIGHTLYBUILD%\include\python373\
+if exist "%NIGHTLYBUILD%\lib\python373\" rmdir /s /q %NIGHTLYBUILD%\lib\python373\
 if not exist "%NIGHTLYBUILD%\lib\python373\" mkdir %NIGHTLYBUILD%\lib\python373\
 
 for /r .\include %%f in (*.h) do @copy "%%f" %NIGHTLYBUILD%\include\python373\
