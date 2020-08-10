245a03b6d89c6c3854caf9ec05720759a2ccf473
Nöthen <Marvin.Noethen@dlr.de>
2020-08-10 09:55:53 +0200

    Some coding style improvements

--- src/module/utilities/pythonextensions/gtpy_loggingmodule.cpp
+++ src/module/utilities/pythonextensions/gtpy_loggingmodule.cpp
@@ -10,8 +10,6 @@
 #include "gtpy_loggingmodule.h"
 using namespace GtpyLoggingModule;
 
-
-
 static void
 GtpyPyLogger_dealloc(GtpyPyLogger* self)
 {
@@ -361,41 +359,41 @@ PyTypeObject
 GtpyLoggingModule::GtpyPyLogger_Type =
 {
     PyVarObject_HEAD_INIT(NULL, 0)
-    "GtLoggingC.GtpyPyLogger",//"GtLoggingC.GtpyPyLogger",             /*tp_name*/
-    sizeof(GtpyPyLogger),             /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)GtpyPyLogger_dealloc, /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,           /*tp_compare*/
-    0,              /*tp_repr*/
-    &GtpyPyLogger_as_number,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,        /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
+    "GtLoggingC.GtpyPyLogger",          /*tp_name*/
+    sizeof(GtpyPyLogger),               /*tp_basicsize*/
+    0,                                  /*tp_itemsize*/
+    (destructor)GtpyPyLogger_dealloc,   /*tp_dealloc*/
+    0,  /*tp_print*/
+    0,  /*tp_getattr*/
+    0,  /*tp_setattr*/
+    0,  /*tp_compare*/
+    0,  /*tp_repr*/
+    &GtpyPyLogger_as_number,  /*tp_as_number*/
+    0,  /*tp_as_sequence*/
+    0,  /*tp_as_mapping*/
+    0,  /*tp_hash */
+    0,  /*tp_call*/
+    0,  /*tp_str*/
+    0,  /*tp_getattro*/
+    0,  /*tp_setattro*/
+    0,  /*tp_as_buffer*/
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    QSTRING_TO_CHAR_PTR(GtpyGlobals::CLASS_GtpyPyLogger + "doc"),           /* tp_doc */
-    0,                   /* tp_traverse */
-    0,                   /* tp_clear */
-    0,                   /* tp_richcompare */
-    0,                   /* tp_weaklistoffset */
-    0,                   /* tp_iter */
-    0,                   /* tp_iternext */
-    0, //GtpyPyLogger_methods,                  /* tp_methods */
-    0,//GtpyLoggingModule_StaticMembers,                   /* tp_members */
-    0,                   /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
-    0,                         /* tp_init */
-    0,                         /* tp_alloc */
-    GtpyPyLogger_new,                 /* tp_new */
+    QSTRING_TO_CHAR_PTR(GtpyGlobals::CLASS_GtpyPyLogger + "doc"), /* tp_doc */
+    0,  /* tp_traverse */
+    0,  /* tp_clear */
+    0,  /* tp_richcompare */
+    0,  /* tp_weaklistoffset */
+    0,  /* tp_iter */
+    0,  /* tp_iternext */
+    0,  /* tp_methods */
+    0,  /* tp_members */
+    0,  /* tp_getset */
+    0,  /* tp_base */
+    0,  /* tp_dict */
+    0,  /* tp_descr_get */
+    0,  /* tp_descr_set */
+    0,  /* tp_dictoffset */
+    0,  /* tp_init */
+    0,  /* tp_alloc */
+    GtpyPyLogger_new,  /* tp_new */
 };
