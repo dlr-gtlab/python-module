7847f01f6d9ffa4bc112356c5259ab929368991b
Nöthen, Marvin <marvin.noethen@dlr.de>
2020-10-05 15:19:05 +0200

    Resolve "Pipeline adjustment"

--- tests/build/build-py37.sh
+++
@@ -1,7 +0,0 @@
-cp features/gitlab_linux_local_settings.pri local_settings.pri
-
-/opt/Qt/5.12.5/gcc_64/bin/qmake gtlab_python.pro -spec linux-g++ CONFIG+=release #CONFIG+=qml_debug
-
-make clean
-
-make
