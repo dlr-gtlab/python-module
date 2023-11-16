a92c0260f3b90a4a07383356e175616fbbf674b0
Nöthen, Marvin <marvin.noethen@dlr.de>
2023-11-16 09:44:55 +0100

    gtpy::convert namespace added

---
+++ src/module/utilities/gtpy_convert.h
@@ -0,0 +1,34 @@
+/* GTlab - Gas Turbine laboratory
+ * Source File: gtpy_convert.h
+ * copyright 2009-2019 by DLR
+ *
+ *  Created on: 15.11.2023
+ *  Author: Marvin Noethen (AT-TW)
+ *  Tel.: +49 2203 601 2692
+ */
+
+#ifndef GTPYCONVERT_H
+#define GTPYCONVERT_H
+
+#include "gt_version.h"
+
+#include <Python.h>
+
+#include "gt_propertystructcontainer.h"
+
+namespace gtpy {
+
+namespace convert {
+
+#if GT_VERSION >= GT_VERSION_CHECK(2, 0, 0)
+PyObject*
+fromPropertyStructContainer(const GtPropertyStructContainer& con);
+#endif // GT_VERSION >= GT_VERSION_CHECK(2, 0, 0)
+
+} // namespace convert
+
+} // namespace gtpy
+
+
+
+#endif // GTPYCONVERT_H
