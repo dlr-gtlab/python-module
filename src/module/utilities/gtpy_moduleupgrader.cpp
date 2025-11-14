29f24f9aaf86d587f02ebcd75c797a4475807240
Schmeink, Jens <Jens.Schmeink@dlr.de>
2025-11-14 13:52:13 +0100

    first version of upgrader

--- src/module/utilities/gtpy_moduleupgrader.cpp
+++ src/module/utilities/gtpy_moduleupgrader.cpp
@@ -12,9 +12,106 @@
 
 #include "gt_logging.h"
 
+namespace {
+
+    void findElementsByClass(const QDomNode& node,
+                             const QStringList& classNames,
+                             QList<QDomElement>& results)
+    {
+        QDomNode child = node.firstChild();
+        while (!child.isNull()) {
+
+            if (child.isElement()) {
+                QDomElement elem = child.toElement();
+
+                if (!elem.isNull()) {
+                    QString classValue = elem.attribute("class");
+
+                    if (classNames.contains(classValue)) {
+                        results.append(elem);
+                        // laut deiner Anforderung:
+                        // solche Einträge haben KEINE weiteren relevanten Untereinträge,
+                        // aber wir durchsuchen trotzdem nicht den Baum *innerhalb* dieses Elements:
+                        child = child.nextSibling();
+                        continue;
+                    }
+                }
+            }
+
+            // rekursiv in die nächste Ebene
+            findElementsByClass(child, classNames, results);
+
+            child = child.nextSibling();
+        }
+    }
+
+    void normalizePropertyContainer(QDomElement container)
+    {
+        QDomNode child = container.firstChild();
+
+        while (!child.isNull()) {
+            if (child.isElement()) {
+                QDomElement prop = child.toElement();
+
+                if (prop.tagName() == "property") {
+
+                    // 1. find sub propety "name"
+                    QDomElement nameElem;
+                    QDomNode sub = prop.firstChild();
+
+                    while (!sub.isNull())
+                    {
+                        if (sub.isElement())
+                        {
+                            QDomElement e = sub.toElement();
+                            if (e.attribute("name") == "name")
+                            {
+                                nameElem = e;
+                                break;
+                            }
+                        }
+                        sub = sub.nextSibling();
+                    }
+
+                    if (!nameElem.isNull())
+                    {
+                        QString newName = nameElem.text().trimmed();
+
+                        // 2. name="UUID" → name="OP"
+                        prop.setAttribute("name", newName);
+
+                        // 3. Remove sub property <property name="name">…</property>
+                        prop.removeChild(nameElem);
+                    }
+                }
+            }
+
+            child = child.nextSibling();
+        }
+    }
+
+}
+
 bool
 gtpy::module_upgrader::to_2_0_0::run(QDomElement& root,
                                      const QString& targetPath)
 {
-    gtDebug() << "Do upgrade for file" << targetPath;
+    // only do updates for process elements
+    if (!targetPath.endsWith(".gttask")) return true;
+
+    if (root.isNull())
+    {
+        gtError() << QObject::tr("Invalid .gttask file!");
+        return false;
+    }
+
+    QList<QDomElement> calcElements;
+    findElementsByClass(root, {"GtpyScriptCalculator", "GtpyTask"}, calcElements);
+
+    for (auto e : calcElements)
+    {
+        normalizePropertyContainer(e);
+    }
+
+    return true;
 }
